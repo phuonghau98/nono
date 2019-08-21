32303304aa645067b1801890b818a4c28b93d9ad
phuonghau98 <hvphuong98@gmail.com>
2019-08-21 10:34:17 +0700

    finished

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+INSTALL_DIR=$HOME/.nono
+nono_has () {
+  type "$1" > /dev/null 2>&1
+}
+
+nono_download() {
+  if nono_has "curl"; then
+    curl --compressed -q "$@"
+  elif nono_has "wget"; then
+    ARGS=$(echo "$*" | command sed -e 's/--progress-bar /--progress=bar /' \
+                            -e 's/-L //' \
+                            -e 's/--compressed //' \
+                            -e 's/-I /--server-response /' \
+                            -e 's/-s /-q /' \
+                            -e 's/-o /-O /' \
+                            -e 's/-C - /-c /')
+    # shellcheck disable=SC2086
+    eval wget $ARGS
+  fi
+}
+mkdir $INSTALL_DIR
+nono_download -s https://raw.githubusercontent.com/phuonghau98/nono/master/nono.sh -o $INSTALL_DIR/nono.sh
+chmod a+x $INSTALL_DIR/nono.sh
+echo -e "alias nono=$INSTALL_DIR/nono.sh" >> /home/$USER/.bashrc
+source $HOME/.bashrc
